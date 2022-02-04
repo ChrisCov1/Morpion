5a749c1b30cc1d6a7ab35b7d323fd651f95827ae
ChrisCov1 <97564841+ChrisCov1@users.noreply.github.com>
2022-02-04 09:35:07 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -10,9 +10,17 @@ require_relative 'show'
 class Application
   def perform
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
-    player1 = Player.new("player1.name", player_symbol)
-    player2 = Player.new("player2.name", player_symbol)
-    
+    puts " Bienvenue joueur 1, quel est votre nom "
+    @name = gets.chomp
+    puts "Bonjour #{@name}"
+    player1 = Player.new(@name, "X")
+
+    puts " Bienvenue joueur 2, quel est votre nom "
+    @name2 = gets.chomp
+    puts "Bonjour #{@name2}"
+    player2 = Player.new(@name2, "O")
+    binding.pry
+
     puts "Jouons ensemble au MORPION"
     while cases.value >= 0 && cases.value <= 8
       puts "on continue de jouer"
@@ -20,4 +28,4 @@ class Application
     end
   end
 end
-#binding.pry
+Application.new.perform
