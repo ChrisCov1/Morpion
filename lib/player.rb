97fb175e5d47b3664991d8ed8c55b5b852dd179a
ChrisCov1 <97564841+ChrisCov1@users.noreply.github.com>
2022-02-04 09:30:39 +0100

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,10 @@
 class Player 
 #TO DO : la classe a 2 attr_reader, son nom et sa valeur (X ou O).
-  attr_accessor :name, :symbol, 
+  attr_accessor :name, :symbol 
 
-  def initialize(name, symbol)#TO DO : doit régler son nom et sa valeur
-    @name = player_name
-    @symbol = player_symbol
+  def initialize(name, symbol) #TO DO : doit régler son nom et sa valeur
+    @name = name
+    @symbol = symbol
     
   end
 end
